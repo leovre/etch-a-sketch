0ce759beb4997ab677569d8409f81a4911d262ec
Leo <leonardovrodriguez246@gmail.com>
2023-04-08 15:59:54 -0400

    adding DOM

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+let box = document.getElementById('box');
+let new_row = document.createElement('div');
+new_row.className= 'square';
+new_row.style.width='16px';
+new_row.style.height='16px';
+new_row.style.border='1px solid black';
+
+box.appendChild(new_row);
+
+
+console.log(container)
+});
